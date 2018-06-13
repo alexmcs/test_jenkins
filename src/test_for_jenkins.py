2ecace2328e3548a95f2af3b8c1955d0e46091c5
Alexey <alexmcs@mail.ru>
2018-06-13 10:53:58 +0300

    Update test_for_jenkins.py

--- src/test_for_jenkins.py
+++ src/test_for_jenkins.py
@@ -4,5 +4,5 @@ def test_job():
     with open("../data/test_jenkins.txt", "a") as myfile:
         myfile.write(str(datetime.datetime.time(datetime.datetime.now())) + "\n")
 
-with open("../data/test_jenkins.txt", "a") as myfile:
+with open("c:/Users/Alexey_Yakunin/A_Yakunin/projects/test_jenkins/data/test_jenkins.txt", "a") as myfile:
     myfile.write(str(datetime.datetime.time(datetime.datetime.now())) + "\n")
